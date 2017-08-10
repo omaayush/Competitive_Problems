9cb4e6af801ac8799c15c525a4659ba9d2b53af4
Aayush Varshney <aayushvarshney1998@gmail.com>
2017-08-10 16:29:41 +0530

    CREATED CODECHEF REPO
    HERE, CODE IN JAVA FROM NETBEANS IDE WILL BE SHOWN..... using jdk 8

---
+++ CodeChef/NetbeansIDE/src/TEST.java
@@ -0,0 +1,26 @@
+/*
+ * My code is awesome.
+ * suggestions to the code will be highly appreciable
+ * Contact me for any query @ omaayushvarshney@gmail.com
+ */
+
+/**
+ *
+ * @author om
+ */
+//LIFE,UNIVERSE & EVERYTHING
+
+class TEST {
+    public static void main(String args[]) throws java.lang.Exception
+    {
+        java.io.BufferedReader r=new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        while(true)
+        {int n=Integer.parseInt(r.readLine());
+            if(n!=42)
+                System.out.println(n);
+            else
+                break;
+        }
+    }
+    
+}
